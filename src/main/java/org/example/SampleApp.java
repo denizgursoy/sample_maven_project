d543b5911602e242c76aedb5f8db4ab362eb8cab
deniz gürsoy <dgursoy@innova.com.tr>
2021-04-23 12:19:00 +0300

    comments were written

--- src/main/java/org/example/SampleApp.java
+++ src/main/java/org/example/SampleApp.java
@@ -1,8 +1,11 @@
 package org.example;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class SampleApp {
 
     public static void main(String[] args) {
-
+        String capitalized = StringUtils.capitalize("sample");
+        System.out.println(capitalized);
     }
 }
